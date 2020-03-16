77bad212010fe4e9b739026361407e87f9085480
yoshi-2015 <yoshihisa.ohkura@icloud.com>
2020-03-16 06:56:15 +0000

    [ADD]文字列/四則演算と割り算の余り表示

---
+++ Main.java
@@ -0,0 +1,15 @@
+class Main { // ここにクラスを記述
+  public static void main(String[] args) { // ここにメゾットを記述
+    System.out.println("Hello Java"); // ここに処理を記述
+
+    System.out.println("Hello Java"); // 文字列
+
+    System.out.println(3); // 数値
+    System.out.println(1 + 2); // 結果：3
+    System.out.println(5 - 2); // 結果：3
+
+    System.out.println(5 * 2); // 結果：10
+    System.out.println(6 / 2); // 結果：3
+    System.out.println(8 % 5); // 結果：3
+  }
+}

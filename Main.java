99f0f39d766eb9a0b6e5cbf849540682ec89c47a
yoshi-2015 <yoshihisa.ohkura@icloud.com>
2020-03-17 05:35:25 +0000

    変数の上書きと注意点/記述コードは上から処理されることの認識強化

--- Main.java
+++ Main.java
@@ -1,8 +1,7 @@
 class Main { // ここにクラスを記述
   public static void main(String[] args) { // ここにメゾットを記述
+    //
     System.out.println("Hello Java"); // ここに処理を記述
-
-    System.out.println("Hello Java"); // 文字列
 
     System.out.println(3); // 数値
     System.out.println(1 + 2); // 結果：3
@@ -12,6 +11,7 @@ class Main { // ここにクラスを記述
     System.out.println(6 / 2); // 結果：3
     System.out.println(8 % 5); // 結果：3
 
+    //
     int a;
     a = 10;
     System.out.println(a); // 結果：10
@@ -20,14 +20,45 @@ class Main { // ここにクラスを記述
     b = "hello,world!"
     System.out.println(b); // 結果："hello,world!"
 
+    //
     int hoge1 = 100;
     System.out.println(hoge1 + 200); // 結果：300
 
     int hoge2 = 10;
     System.out.println(hoge1 + hoge2); // 結果：110
 
+    //
     String name1 = "Yamada";
     String name2 = "Taro";
     System.out.println(name1 + name2); // 結果：YamadaTaro
+
+    //
+    int number = 11;
+    String text = "Ruby";
+    System.out.println(number);
+    System.out.println(text);
+
+    // 変数numberを9で上書きしてください
+    number = 9;
+
+    // 変数numberを出力してください
+    System.out.println(number);
+
+    // 変数textを「Java」で上書きしてください
+    text = "Java";
+
+    // 変数textを出力してください
+    System.out.println(text);
+
+
+    //
+  }
+}
+
+
+
+class Main { // ここにクラスを記述
+  public static void main(String[] args) { // ここにメゾットを記述
+
   }
 }

4d75f757c8d53bcc5247d791497f04118ea7d2ac
yoshi-2015 <yoshihisa.ohkura@icloud.com>
2020-03-17 04:53:32 +0000

    [ADD]変数の初期化/変数を用いたint型の計算とString型の連結

--- Main.java
+++ Main.java
@@ -19,5 +19,15 @@ class Main { // ここにクラスを記述
     String b;
     b = "hello,world!"
     System.out.println(b); // 結果："hello,world!"
+
+    int hoge1 = 100;
+    System.out.println(hoge1 + 200); // 結果：300
+
+    int hoge2 = 10;
+    System.out.println(hoge1 + hoge2); // 結果：110
+
+    String name1 = "Yamada";
+    String name2 = "Taro";
+    System.out.println(name1 + name2); // 結果：YamadaTaro
   }
 }

ca19359459e1d5dcbaa8d7f5840ace27aa1aaddb
yoshi-2015 <yoshihisa.ohkura@icloud.com>
2020-03-21 11:22:04 +0000

    [ADD]真偽値について/比較演算子の導入

---
+++ Main2.java
@@ -0,0 +1,35 @@
+class Main {
+  public static void main(String[] args) {
+
+    //
+    // 真偽値
+    // 真偽値のデータ型はboolean型
+    // 真偽値には「true」と「false」という2つの値しか存在しない
+    // true, falseにはダブルクォーテーションは付けない（つけると文字列型になる）
+    boolean bool = true; // boolという変数がtrue判定
+
+    //
+    // 比較演算子
+    // 値を比較するための記号で、比較した結果は真偽値になる
+    x == y // 等しいか調べる（xとyが等しいならture,xとyが等しくないならfalse）
+    x != y // 等しくないか調べる（xとyが等しくないならture,xとyが等しいならfalse）
+
+
+
+  }
+}
+
+
+
+
+
+
+```java:Main.java
+class Main {
+  public static void main(String[] args) {
+
+    
+
+  }
+}
+```
